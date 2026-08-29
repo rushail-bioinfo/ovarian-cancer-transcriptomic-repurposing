7ebf8dac79e82a2ba2b64a2f96dcac6e7aa2a3e5
rushail <wishyyy0901@gmail.com>
2026-08-30 01:40:38 +0500

    Day 3: DE analysis on primary dataset (log2-corrected), 7942 significant genes, volcano plot

--- 01_data_pull.R
+++ 01_data_pull.R
@@ -1,14 +1,8 @@
-library(GEOquery)
+library(Biobase)
+library(limma)
 
-gse_primary <- getGEO("GSE18520", GSEMatrix = TRUE)
-gse_primary <- gse_primary[[1]]
+expr_primary <- exprs(gse_primary)
+library(Biobase)
+library(limma)
 
-gse_validation <- getGEO("GSE14407", GSEMatrix = TRUE)
-gse_validation <- gse_validation[[1]]
-
-table(pData(gse_primary)$characteristics_ch1)
-table(pData(gse_validation)$characteristics_ch1)
-
-dim(exprs(gse_primary))
-dim(exprs(gse_validation))
-
+expr_primary <- exprs(gse_primary)
